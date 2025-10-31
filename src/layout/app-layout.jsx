ec7ecda013789ea7ae4e67b0422558e591a23a4a
Abhilash-0322 <mauryaabhi2003@gmail.com>
2025-10-31 19:47:15 +0530

    margin fix

--- src/layout/app-layout.jsx
+++ src/layout/app-layout.jsx
@@ -4,9 +4,9 @@ import Header from '@/components/header'
 
 function AppLayout() {
   return (
-    <div>
+    <div className='relative'>
       <div className='grid-background'></div>
-      <main className='min-h-screen container'>
+      <main className='min-h-screen container px-4 sm:px-6 lg:px-8'>
         <Header/>
         <Outlet/>
       </main>
